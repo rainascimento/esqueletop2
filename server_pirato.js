a31a367cba26ee926f7bf12290813b37a0068207
Rai Nascimento <rai.nascimento.silva@gmail.com>
2021-08-02 22:42:55 -0300

    Esqueletop 2

--- server_pirato.js
+++ server_pirato.js
@@ -1,5 +1,5 @@
 const http = require('http'); // importamos o http -- não se importe muito com isso agora 
 const app = require('./app'); // aqui estamos pegando a exportação que fizemos no final do app.js
-const port = process.env.PORT || 3000; // falamos qaul a porta que vamos trabalhar. Se a variavel de ambiente não existir ele vai de 3000 por padrão
+const port = process.env.PORT || 8080; // falamos qaul a porta que vamos trabalhar. Se a variavel de ambiente não existir ele vai de 3000 por padrão
 const server = http.createServer(app);// aqui criamos o sevidor 
 server.listen(port); //e falamos que o servidor vai escutar a porta que armazenamos na constante port. 
